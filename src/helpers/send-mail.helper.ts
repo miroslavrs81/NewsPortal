709d39f475bbdc5ab6d55b73c5c8d25dab77d702
miroslavrs81 <miroslav81ilic@gmail.com>
2024-01-04 18:24:54 +0100

    Send Mail Helper

---
+++ src/helpers/send-mail.helper.ts
@@ -0,0 +1,37 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { emailLogger } from './winston-logger.helper';
+
+export function sendMail(
+  email: string,
+  subject: string,
+  template: string,
+  context: object,
+  mailerService: MailerService,
+  attachments: { filename: string; path: string }[] = [],
+): Promise<boolean> {
+  return mailerService
+    .sendMail({
+      to: email,
+      from: process.env.APP_EMAIL,
+      subject,
+      template,
+      context,
+      attachments,
+    })
+    .then((response) => {
+      emailLogger.log({
+        level: 'info',
+        message: JSON.stringify(response),
+        type: subject,
+      });
+      return true;
+    })
+    .catch((error) => {
+      emailLogger.log({
+        level: 'error',
+        message: JSON.stringify(error.message),
+        type: subject,
+      });
+      return false;
+    });
+}
