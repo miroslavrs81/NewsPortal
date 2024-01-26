552bd16d94a9b2f33e83d271a4c3bd1da16e106d
miroslavrs81 <miroslav81ilic@gmail.com>
2024-01-26 21:59:48 +0100

    Fix 2 Naming Module

--- src/admin/admin-main.module.ts
+++ src/admin/admin-main.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AdminUserModule } from './user/admin-user.module';
-import { AdminCategoryModule } from './categories/admin-category.module';
+import { AdminCategoryModule } from './category/admin-category.module';
 import { AdminNewsModule } from './news/admin-news.module';
 import { PageModule } from './page/admin-page.module';
 
