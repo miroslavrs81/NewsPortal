ea2bef2cdf023c73921daa3cc0c5ed5a8b114391
miroslavrs81 <miroslav81ilic@gmail.com>
2024-11-02 13:48:44 +0100

    Page Profile Entity

--- src/entities/page-profile.entity.ts
+++ src/entities/page-profile.entity.ts
@@ -1,11 +1,8 @@
-import { IsEmail, IsUrl } from 'class-validator';
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
-  CreateDateColumn,
   UpdateDateColumn,
-  DeleteDateColumn,
 } from 'typeorm';
 
 @Entity({ name: 'page_profiles' })
@@ -13,33 +10,24 @@ export class PageProfile {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 30 })
-  firstName: string;
+  @Column({ type: 'varchar', length: 255 })
+  title: string;
 
-  @Column({ type: 'varchar', length: 30 })
-  lastName: string;
+  @Column({ type: 'text' })
+  content: string;
 
-  @Column({ type: 'varchar', length: 30 })
-  position: string;
+  @Column({ type: 'json', nullable: true })
+  metadata: {
+    teamMembers?: Array<{
+      firstName: string;
+      lastName: string;
+      position: string;
+      email: string;
+      githubProfile: string;
+      linkedInProfile: string;
+    }>;
+  };
 
-  @IsEmail()
-  @Column({ type: 'varchar', unique: true, length: 50 })
-  email: string;
-
-  @IsUrl()
-  @Column({ type: 'varchar', length: 50 })
-  githubProfile?: string;
-
-  @IsUrl()
-  @Column({ type: 'varchar', length: 50 })
-  linkedInProfile?: string;
-
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
+  @UpdateDateColumn({ type: 'timestamp' })
   updatedAt: Date;
-
-  @DeleteDateColumn()
-  deletedAt: Date;
 }
