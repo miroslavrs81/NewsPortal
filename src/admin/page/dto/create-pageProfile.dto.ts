b429fbcef1bf8d1de33c332d6490fc9e31db6886
miroslavrs81 <miroslav81ilic@gmail.com>
2024-11-02 13:53:30 +0100

    Create Page Profile Dto fix

--- src/admin/page/dto/create-pageProfile.dto.ts
+++ src/admin/page/dto/create-pageProfile.dto.ts
@@ -1,26 +1,51 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
+import { IsArray, IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
 
-export class CreatePageProfileDto {
+export class TeamMember {
   @ApiProperty()
   @IsString()
   firstName: string;
 
+  @ApiProperty()
   @IsString()
   lastName: string;
 
+  @ApiProperty()
   @IsString()
   position: string;
 
+  @ApiProperty()
   @IsEmail()
-  @IsString()
   email: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsUrl()
   githubProfile?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsUrl()
   linkedInProfile?: string;
+}
+
+export class Metadata {
+  @ApiProperty({ type: [TeamMember], required: false })
+  @IsOptional()
+  @IsArray()
+  teamMembers?: TeamMember[];
+}
+
+export class CreatePageProfileDto {
+  @ApiProperty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  content: string;
+
+  @ApiProperty({ type: Metadata, required: false })
+  @IsOptional()
+  metadata?: Metadata;
 }
